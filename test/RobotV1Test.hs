e57496002b5803c0dfad8a85051cc0154984d6c0
futtetennista <futtetennista@pm.me>
2022-08-06 11:43:38 +0100

    Clean ups

--- test/RobotV1Test.hs
+++ test/RobotV1Test.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module RobotV1Test (tests) where
@@ -12,6 +11,7 @@ import RobotV1
     moveRobot,
   )
 import qualified Test.Tasty as T
+import Test.Tasty.ExpectedFailure (ignoreTest)
 import qualified Test.Tasty.HUnit as H
 
 -- import qualified Test.Tasty.QuickCheck as QC
@@ -20,18 +20,20 @@ tests :: T.TestTree
 tests =
   T.testGroup
     "Tests"
-    [ H.testCase "Test mars rover found" do
+    [ H.testCase "Mars rover found" do
         let moves = [L, F, R, F, F]
             grid = (4, 8)
             initRobot = Robot (2, 3) E
             actual = moveRobot moves grid (Found initRobot)
             expected = Found (Robot (4, 4) E)
         actual H.@?= expected,
-      H.testCase "Test it returns a spelled number" do
+      H.testCase "Mars rover lost" do
         let moves = [F, F, L, F, R, F, F]
             grid = (4, 8)
             initRobot = Robot (0, 2) N
             actual = moveRobot moves grid (Found initRobot)
             expected = Lost (Robot (0, 4) W)
-        actual H.@?= expected
+        actual H.@?= expected,
+      ignoreTest $ H.testCase "Mars rover movements" do
+        H.assertBool "" False
     ]
